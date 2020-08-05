348c2ec6ec3a5820b1f5e2a06c960a2d2af2c5b1
torijacarlos <hi@torijacarlos.com>
2020-08-05 12:49:31 -0500

    fix: workflow snippet and string comparation fixed

--- validation/git-consistency.sh
+++ validation/git-consistency.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $PWD
+
 BASE_HEAD=$(git rev-parse --short ${BASE_BRANCH:-'origin/master'})
 CURRENT_HEAD=$(git rev-parse --short HEAD)
 DIFF_INFO=$(git diff --shortstat $BASE_HEAD..$CURRENT_HEAD ':(exclude)*.lock' ':(exclude)package-lock.json')
@@ -9,14 +11,13 @@ HARD_LIMIT_INSERTIONS=200
 SOFT_LIMIT_DELETIONS=300
 HARD_LIMIT_DELETIONS=400
 
-if [[ $repo_type -eq "mobile" ]]; then
+if [[ $REPO_TYPE == "mobile" ]]; then
     SOFT_LIMIT_INSERTIONS=300
     HARD_LIMIT_INSERTIONS=400
     SOFT_LIMIT_DELETIONS=600
     HARD_LIMIT_DELETIONS=800
 fi
   
-
 # number of commits
 
 NUMBER_OF_COMMITS=$(git rev-list --count $BASE_HEAD..$CURRENT_HEAD)
@@ -35,8 +36,8 @@ if [[ $NUMBER_OF_INSERTIONS -gt $HARD_LIMIT_INSERTIONS ]]; then
     exit 1
 fi
 
-if [[ $NUMBER_OF_DELETIONS -gt $SOFT_LIMIT_DELETIONS ]]; then
-    echo "ERROR #3: There are more than $SOFT_LIMIT_DELETIONS deletions"
+if [[ $NUMBER_OF_DELETIONS -gt $HARD_LIMIT_DELETIONS ]]; then
+    echo "ERROR #3: There are more than $HARD_LIMIT_DELETIONS deletions"
     exit 1
 fi
 
@@ -50,3 +51,4 @@ if [[ $NUMBER_OF_DELETIONS -gt $SOFT_LIMIT_DELETIONS ]]; then
     echo "WARNING #2: There are more than $SOFT_LIMIT_DELETIONS insertions"
 fi
 
+echo "Success: This branch is ready to be merged!"
