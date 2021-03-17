740ff4fe2dd258b4cc2b6ebc125405fb983c318c
Froylán Sánchez <froy@fondeadora.com>
2021-03-17 16:56:57 -0600

    chore: wtf github

--- common/git-setup.sh
+++ common/git-setup.sh
@@ -1,1 +1,6 @@
-sudo git config --global url."https://$ACCESS_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
+cat .git/config
+git config --global url."https://$ACCESS_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
+git config --list --show-origin
+cat .git/config
+cat /etc/gitconfig
+cat /home/runner/.gitconfig
