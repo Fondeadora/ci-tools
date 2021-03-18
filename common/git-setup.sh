79606a9b1b61c25b5d48fd165b29ca7d747ccde0
Cristian Benavides Jimenez <sairoko@MacBook-Pro-de-Cristian.local>
2021-03-18 11:52:00 -0600

    chore: set hardcode access token

--- common/git-setup.sh
+++ common/git-setup.sh
@@ -1,23 +1,13 @@
-git config credential.helper store --file .git/credentials
-git config --list --show-origin
-echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
+git config credential.helper store
+
+# echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
 echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a ~/.git-credentials
-echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a .git/credentials
+# echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a .git/credentials
 
-echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a /etc/gitconfig
+# echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a /etc/gitconfig
 echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a ~/.gitconfig
-echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a .git/config
+# echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a .git/config
 
-echo "/etc/gitconfig"
-cat /etc/gitconfig
-echo ".git/config"
-cat .git/config
-echo "~/.gitconfig"
-cat ~/.gitconfig
-
-echo "/etc/git-credentials"
-cat /etc/git-credentials
-echo "~/.git-credentials"
-cat ~/.git-credentials
-echo ".git/credentials"
-cat .git/credentials
+# echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git+ssh://git@github.com/" | sudo tee -a /etc/gitconfig
+echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git+ssh://git@github.com/" | sudo tee -a ~/.gitconfig
+# echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git+ssh://git@github.com/" | sudo tee -a .git/config
