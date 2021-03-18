c3e1a50109f994bc96795e2dfbaa58372acbf20b
miguelangelduarte <miguel.duarte.111895@gmail.com>
2021-03-18 11:05:22 -0600

    refactor: add credentials helper store

--- common/git-setup.sh
+++ common/git-setup.sh
@@ -1,8 +1,23 @@
-cat .git/config
-echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a /etc/gitconfig
+git config credential.helper store --file .git/credentials
 git config --list --show-origin
-cat .git/config
+echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
+echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a ~/.git-credentials
+echo -e "https://tech-fondeadora:$ACCESS_TOKEN@github.com" | sudo tee -a .git/credentials
+
+echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a /etc/gitconfig
+echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a ~/.gitconfig
+echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a .git/config
+
+echo "/etc/gitconfig"
 cat /etc/gitconfig
-cat /home/runner/.gitconfig
-echo -e "https://$GIT_USER_NAME:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
+echo ".git/config"
+cat .git/config
+echo "~/.gitconfig"
+cat ~/.gitconfig
+
+echo "/etc/git-credentials"
 cat /etc/git-credentials
+echo "~/.git-credentials"
+cat ~/.git-credentials
+echo ".git/credentials"
+cat .git/credentials
