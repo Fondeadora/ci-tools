42d6f2e69d4e1fdd09383fc7be19229a9d094bde
palafoxernesto <palafox.ernesto@gmail.com>
2021-03-18 10:49:57 -0600

    refactor: add git-credentials file

--- common/git-setup.sh
+++ common/git-setup.sh
@@ -3,3 +3,6 @@ echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@gi
 git config --list --show-origin
 cat .git/config
 cat /etc/gitconfig
+cat /home/runner/.gitconfig
+echo -e "https://$GIT_USER_NAME:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
+cat /etc/git-credentials
