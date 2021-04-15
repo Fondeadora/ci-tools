fde477f4d1d8a1359e7a408eba4d445bd9f9e0aa
gonz <gonzasestopal@gmail.com>
2021-04-15 11:43:00 -0500

    ci: use pip3 default

--- common/python-setup.sh
+++ common/python-setup.sh
@@ -2,4 +2,4 @@ for VERSION in "/opt/hostedtoolcache/Python/$PYTHON_VERSION"*; do
     export PATH="$VERSION/x64/bin:$PATH"
     echo "$VERSION/x64/bin" >> $GITHUB_PATH
 done
-pip3.7 install -q pipenv
+pip3 install -q pipenv
